18b7137258978abd0e40bf1ad3d33d138a8f89a6
rafaeldev1 <rafaeleligeralves@gmail.com>
2020-06-04 01:53:33 -0300

    Primeiro commit

---
+++ scripts/intro.js
@@ -0,0 +1,69 @@
+// document.write("Hello")
+
+// variaveis, tipos de dados-----------
+
+// sem valor = undefined
+// var myvar = "Do"
+// const myconst = "Re"
+// let mylet = "Mi"
+// document.write(myconst + myvar)
+
+//string------------------------------
+
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso tambem é uma string"
+
+//number-------------------------------
+
+// const n1 = 1
+// const n2 = 31
+// document.write(n1 + n2)
+
+//boolean-------------------------------
+
+// true or false
+// const bTrue = true
+// const bFalse - false
+
+// objeto-------------------------------
+// todos possuem propriedade e funcionalidade
+
+// const pessoa = {
+  // altura: "1,80m",
+  // idade: 24,
+  // solteiro: true,
+  // correr(){
+    // return "run forest"
+  // }
+// }
+
+// document.write(pessoa.correr())
+
+// array---------------------------vetores------
+// const colecaodebolinhas = ["blue", "green", 1)
+// document.write(colecaodebolinhas[0])
+
+// Funções - atalhos, funcionalidade------------
+
+// registrar função
+// function sayMyName(name){
+  // document.write(name)
+// }
+
+// execução de função
+// sayMyName("Mayk")
+// sayMyName("Luka")
+// sayMyName("Rafael")
+
+// const notaFinal = 2
+
+// if(notaFinal < 6 ){ //caminho 1
+  // document.write("Reprovado")
+// } else {  //caminho 2
+  // document.write("Aprovado")
+// }
+
+//loop repetição ----------------------------
+//for (i = 0; i < 10; i++){
+//    document.write(`<p>${i}</p>`)
+//  }
