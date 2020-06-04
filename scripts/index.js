aabd7d2fb7d203a1aa738c287b07375706ac4418
rafaeldev1 <rafaeleligeralves@gmail.com>
2020-06-04 19:18:36 -0300

    Segundo commit

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const buttonSerach = document.querySelector("#page-home main a")
+const modal = document.querySelector("#modal")
+const close = document.querySelector("#modal .header a")
+
+buttonSerach.addEventListener("click", () => {
+    modal.classList.remove("hide")
+})
+
+close.addEventListener("click", () => {
+    modal.classList.add("hide")
+})
+
